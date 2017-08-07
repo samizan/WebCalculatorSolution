cc65d671567544e36e6da348fea40d617d6c80d1
Samer Azani <samer.azani@gmail.com>
2017-08-07 23:12:06 +0200

    Simple Web Calculator
    A simple web calculator using Javascript, closures, and HTML

---
+++ WebCalculator/Scripts/CalculatorLibrary.js
@@ -0,0 +1,126 @@
+﻿/// <reference path="_references.js" />
+
+(function () {
+    this.calculatorNamespace = this.calculatorNamespace || {};
+    var ns = this.calculatorNamespace;
+
+     ns.initialize = function () {
+         var calculator = new ns.Calculator();
+
+        $('button[id^="btnNumber"]').on('click', calculator.numberClick);
+        $('#btnPlus').on('click', calculator.operatorClick("+"));
+        $('#btnMinus').on('click', calculator.operatorClick("-"));
+        $('#btnDivide').on('click', calculator.operatorClick("/"));
+        $('#btnMultiply').on('click', calculator.operatorClick("*"));
+        $('#btnEqual').on('click', calculator.equalClick);
+        $('#btnClearEntry').on('click', calculator.clearEntry);
+        $('#btnClear').on('click', calculator.clear);
+        calculator.clear();
+
+
+    }
+
+    ns.Calculator = (function () {
+        var operation = "";
+        var lastOperation = "";
+
+        function Calculator() {
+        }
+
+        Calculator.prototype.clear = function () {
+            $('#txtInput').val('0');
+            $('#txtResult').val('0');
+        }
+
+         Calculator.prototype.numberClick = function () {
+             $('#txtInput').val($('#txtInput').val() == '0' ?
+                 $(this).text() : $('#txtInput').val() + $(this).text());
+        }
+
+
+         Calculator.prototype.operatorClick = function (calledOperation) {
+             return function () {
+
+                 if (operation != calledOperation && operation != "") {
+                     Calculator.prototype.runPreviousOperator(operation);
+                 }
+                 operation = calledOperation;
+                 selectOperation();
+             }
+         }
+
+
+        Calculator.prototype.clearEntry = function() {
+            $('#txtInput').val('0');
+        }
+
+        Calculator.prototype.clearClick = function() {
+            $('#txtResult').val('0');
+            Calculator.prototype.clearEntry();
+        }
+
+        Calculator.prototype.runPreviousOperator = function () {
+            selectOperation();
+        }
+
+        Calculator.prototype.equalClick = function () {
+            lastOperation = "=";
+            selectOperation();
+            
+        }
+
+        function selectOperation() {
+            switch (operation) {
+                case "+": Calculator.prototype.addNumbers();
+                    break;
+                case "-": Calculator.prototype.subtractNumbers();
+                    break;
+                case "/": Calculator.prototype.divideNumbers();
+                    break;
+                case "*": Calculator.prototype.multiplyNumbers();
+                    break;
+            }
+
+            Calculator.prototype.clearEntry();
+        }
+
+        Calculator.prototype.addNumbers = function () {
+            $('#txtResult').val(Number($('#txtResult').val()) +
+               Number($('#txtInput').val()));
+        }
+
+        Calculator.prototype.subtractNumbers = function () {
+            $('#txtResult').val(Number($('#txtResult').val()) -
+               Number($('#txtInput').val()));
+        }
+
+        Calculator.prototype.divideNumbers = function () {
+            if ($('#txtInput').val() == "0" && lastOperation == "=") return;
+
+            var result = $('#txtResult').val();
+
+            if ($('#txtResult').val() == 0) {
+                $('#txtResult').val(Number($('#txtInput').val()));
+            }
+            else {
+                $('#txtResult').val(result / $('#txtInput').val());
+            }
+        }
+
+        Calculator.prototype.multiplyNumbers = function () {
+            if ($('#txtInput').val() == "0" && lastOperation == "=") return;
+
+            var result = $('#txtResult').val();
+
+            if ($('#txtResult').val() == 0) {
+                $('#txtResult').val(Number($('#txtInput').val()));
+            }
+            else {
+                $('#txtResult').val(result * $('#txtInput').val());
+            }
+        }
+
+        return Calculator;
+    }());
+
+})();
---
+++ WebCalculator/Scripts/tests.js
@@ -0,0 +1,74 @@
+﻿/// <reference path="_references.js" />
+
+module('Calculator Test Suite', {
+    setup: function () {
+        calculatorNamespace.initialize();
+    }
+});
+
+test("Initialize Test", function () {
+    expect(2);
+    var expected = '0';
+    equal($('#txtInput').val(), expected, 'Expected value: ' + expected + ' Actual value: ' + $('#txtInput').val());
+    equal($('#txtResult').val(), expected, 'Expected value: ' + expected + ' Actual value: ' + $('#txtResult').val());
+});
+
+test("Button Click Test", function () {
+    var buttonQuantity = 10;
+    expect(buttonQuantity * 2);
+    for (var i = 0; i < buttonQuantity ; i++) {
+        $('#btnNumber' + i).triggerHandler('click');
+        var result = $('#txtInput').val()[$('#txtInput').val().length - 1];
+        var expected = String(i);
+        equal(result, expected, 'Expected value: ' + expected + ' Actual value: ' + result);
+        var expectedLength = i < 2 ? 1 : i;
+        equal($('#txtInput').val().length, expectedLength,
+            'Expected String Length: ' + expectedLength,
+            'Actual value: ' + $('#txtInput').val().length);
+    }
+});
+
+test("Add Test", function () {
+    expect(2);
+    var btnPlus = document.getElementById('btnPlus');
+    txtInput.value = '10';
+    txtResult.value = '20';
+    QUnit.triggerEvent(btnPlus, "click");
+    var expected = "30";
+    equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+    expected = '0';
+    equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+
+});
+
+test("Minus Test", function () {
+    expect(2);
+    var btnMinus = document.getElementById('btnMinus');
+    txtInput.value = '10';
+    txtResult.value = '20';
+    QUnit.triggerEvent(btnMinus, "click");
+    var expected = "10";
+    equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+    expected = '0';
+    equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+});
+
+test("Clear Entry Test", function () {
+    expect(1);
+    var btnClearEntry = document.getElementById('btnClearEntry');
+    txtInput.value = '10';
+    QUnit.triggerEvent(btnClearEntry, "click");
+    var expected = "0";
+    equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+});
+
+test("Clear Test", function () {
+    expect(2);
+    var btnClear = document.getElementById('btnClear');
+    txtInput.value = '10';
+    txtResult.value = '20';
+    QUnit.triggerEvent(btnClear, "click");
+    var expected = "0";
+    equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+    equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+});
